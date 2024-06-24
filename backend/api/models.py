5ad348c6e6651605248a564a9aec5b9d7ca1b2d4
tanmayidev <tanmayi.d.99@gmail.com>
2024-06-24 22:40:32 +0530

    feat: create student model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+  first_name = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+  age = models.IntegerField()
+  gender = models.CharField(max_length=6)
+  grade = models.CharField(max_length=2)
+  address = models.TextField()
+  contact_number = models.CharField(max_length=15)
