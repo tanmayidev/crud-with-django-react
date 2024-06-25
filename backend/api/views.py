d383c8168afade953f0439592dbc9bcd39aff331
tanmayidev <tanmayi.d.99@gmail.com>
2024-06-25 10:45:11 +0530

    feat: create 2 views for student

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import Student
+from .serializer import StudentSerializer
+
+class StudentListCreateView(generics.ListCreateAPIView):
+  queryset = Student.objects.all()
+  serializer_class = StudentSerializer
+
+class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Student.objects.all()
+  serializer_class = StudentSerializer
